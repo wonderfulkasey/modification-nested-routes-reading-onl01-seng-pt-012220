8dbe9103ce32ff159c8e0e029ebd777bde74381d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-23 23:29:21 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,22 +2,32 @@ class PostsController < ApplicationController
 
   def index
     if params[:author_id]
-      @posts = Author.find(params[:author_id]).posts
-    else
+@posts = Author.find(params[:author_id]).posts
+  else
       @posts = Post.all
     end
   end
 
   def show
     if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
+      author = Author.find_by(id: params[:author_id])
+            if author.nil?
+              redirect_to authors_path, alert: "Author not found."
+            else
+              @post = author.posts.find_by(id: params[:id])
+              redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+            end
     else
       @post = Post.find(params[:id])
     end
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
   end
 
   def create
@@ -39,6 +49,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
