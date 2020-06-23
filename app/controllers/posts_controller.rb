1754f502b760b134d4c29bc8817366825cd0a7b4
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-23 23:25:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,7 @@ class PostsController < ApplicationController
 
   def index
     if params[:author_id]
-      author = Author.find_by(id: params[:author_id])
-            if author.nil?
-              redirect_to authors_path, alert: "Author not found."
-            else
-              @post = author.posts.find_by(id: params[:id])
-              redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
-            end
+      
   else
       @posts = Post.all
     end
@@ -16,7 +10,13 @@ class PostsController < ApplicationController
 
   def show
     if params[:author_id]
-      @post = Author.find(params[:author_id]).posts.find(params[:id])
+      author = Author.find_by(id: params[:author_id])
+            if author.nil?
+              redirect_to authors_path, alert: "Author not found."
+            else
+              @post = author.posts.find_by(id: params[:id])
+              redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+            end
     else
       @post = Post.find(params[:id])
     end
