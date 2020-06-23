381d1a5756da835269b90b08f44d66a518ea8507
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-23 23:28:44 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :authors, only: [:show, :index] do
+      resources :posts, only: [:show, :index, :new, :edit]
+    end
+
+    resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
